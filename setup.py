9db7682bfabaf42674fae3cab328c0d6b36395d3
pruebando <75038694+pruebando@users.noreply.github.com>
2021-04-17 20:10:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
   packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+  description = 'Api Wrapper Of Jspaste, https://jspaste.tnfangel.repl.co/",   # Give a short description about your library
   author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/pruebando/jspaste/archive/refs/tags/0.0.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+ 
+  url = 'https://github.com/pruebando/jspaste',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/pruebando/jspaste/archive/refs/tags/0.1.tar.gz',    # I explain this later on
+  keywords = ['Pastebin', 'Jspaste', 'Api Wrapper'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests'
       ],
@@ -17,10 +17,13 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'License ::  MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
   ],
 )
