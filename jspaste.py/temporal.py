28a03fe8394193f663da88edfd1525dc3c36ac83
pruebando <75038694+pruebando@users.noreply.github.com>
2021-04-18 11:37:09 +0200

    Update and rename src/temporal.py to jspaste.py/temporal.py

---
+++ jspaste.py/temporal.py
@@ -0,0 +1,3 @@
+from .vares import *
+from .errors import InvalidArgs, NotArgs, UnknownError
+import requests
--- src/temporal.py
+++
@@ -1,3 +0,0 @@
-from jspaste.vares import *
-from jspaste.errors import InvalidArgs, NotArgs, UnknownError
-import requests
